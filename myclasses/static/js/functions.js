061abd50c590284dd8df45c25a45fd356a32cac5
nick!! <113379836+nickmp4@users.noreply.github.com>
2025-01-26 04:41:57 -0600

    uploading works now

--- myclasses/static/js/functions.js
+++ myclasses/static/js/functions.js
@@ -1,9 +1,3 @@
-const syllabusButton = document.getElementById('syllabus_button');
-    const fileChosen = document.getElementById('file_chosen');
-    syllabusButton.addEventListener('change', function() {
-        fileChosen.textContent = this.files[0].name;
-    });
-
 function openNav() {
     document.getElementById("collapsableSidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -12,3 +6,8 @@ function closeNav() {
     document.getElementById("collapsableSidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 };
+
+document.getElementById('syllabus').addEventListener('change', function() {
+    let fileName = this.files[0].name ? this.files[0].name : 'No file chosen';
+    document.getElementById('fileName').textContent = 'Selected file' + fileName;
+});
