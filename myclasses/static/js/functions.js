1c6db4de6e640764edf989231624f9e984fa888e
nick!! <113379836+nickmp4@users.noreply.github.com>
2025-01-26 04:14:43 -0600

    upload test

--- myclasses/static/js/functions.js
+++ myclasses/static/js/functions.js
@@ -1,3 +1,9 @@
+const syllabusButton = document.getElementById('syllabus_button');
+    const fileChosen = document.getElementById('file_chosen');
+    syllabusButton.addEventListener('change', function() {
+        fileChosen.textContent = this.files[0].name;
+    });
+
 function openNav() {
     document.getElementById("collapsableSidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
