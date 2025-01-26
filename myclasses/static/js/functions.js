60852b39f73d22ee4058ccda1206b89780f36e4f
nick!! <113379836+nickmp4@users.noreply.github.com>
2025-01-26 00:01:53 -0600

    revamped sidebar and created js file

---
+++ myclasses/static/js/functions.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("collapsableSidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+};
+function closeNav() {
+    document.getElementById("collapsableSidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+};
